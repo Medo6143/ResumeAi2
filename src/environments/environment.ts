c919ed3a4c74ff465a135cef260becdeb1583ba4
medo6143 <medow6143>
2026-05-19 15:01:08 +0300

    add environment to gitignore and remove from tracking

--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: false,
-    OPENROUTER_API_KEY: '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',
-    GOOGLE_VISION_API_KEY: 'AIzaSyB0xgaq03Kqcpf88-80h0jpOoZXaP_RBaU'
-};
